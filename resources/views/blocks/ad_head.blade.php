b80d4fb92fa0fea105f2d806cca82194d980c7e2
Yakuza <admin@rm-net.si>
2022-04-25 21:43:12 +0200

    ..::SLOshare::.. - 25.04.2022_21:43

--- resources/views/blocks/ad_head.blade.php
+++ resources/views/blocks/ad_head.blade.php
@@ -1,5 +1,14 @@
     <div class="col-md-10 col-sm-10 col-md-offset-1">
         <div class="panel-slo">
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5654650999587494"
+     crossorigin="anonymous"></script>
+<!-- head -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5654650999587494"
+     data-ad-slot="5408044625"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         </div>
     </div>
--- resources/views/blocks/ad_right.blade.php
+++ resources/views/blocks/ad_right.blade.php
@@ -1,5 +1,14 @@
     <div class="col-md-1 col-sm-1-slo col-md-br-1 col-slo-b">
         <div class="panel-slo">
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5654650999587494"
+     crossorigin="anonymous"></script>
+<!-- right -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:293px;height:594px"
+     data-ad-client="ca-pub-5654650999587494"
+     data-ad-slot="2241156938"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         </div>
     </div>
