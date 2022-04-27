847ac3faa19f222d334b78e219bc60f73c1d11fd
Yakuza <admin@rm-net.si>
2022-04-27 09:53:46 +0200

    ..::SLOshare::.. - 27.04.2022_09:53

--- resources/views/page/donationslo.blade.php
+++ resources/views/page/donationslo.blade.php
@@ -30,7 +30,14 @@
                 <h2></h2>
                     <div class="col-xs-6 col-sm-4 col-md-3">
                         <div class="text-center black-item">
-                            <span></span>
+                            <h4>{{ $client }}</h4>
+                            <span>
+                            <form action="https://www.paypal.com/donate" method="post" target="_top">
+                            <input type="hidden" name="hosted_button_id" value="9VSKWN3QQJCQU" />
+                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                            <img alt="" border="0" src="https://www.paypal.com/en_SI/i/scr/pixel.gif" width="1" height="1" />
+                            </form>
+                            </span>
                             <i class="fal fa-ban text-red black-icon"></i>
                         </div>
                     </div>
