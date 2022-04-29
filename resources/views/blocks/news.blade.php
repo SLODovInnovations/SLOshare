fdee27fdc4805bfa53c47b584db319b76eea462a
Yakuza <admin@rm-net.si>
2022-04-29 19:22:04 +0200

    ..::SLOshare::.. - 29.04.2022_19:22

--- resources/views/blocks/news.blade.php
+++ resources/views/blocks/news.blade.php
@@ -1,31 +1,19 @@
 <div class="col-md-3 col-sm-3-slo col-md-bl-1 col-slo">
 @foreach ($articles as $article)
-        @if ($article->newNews)
             <div class="panel panel-danger">
                 <div class="panel-heading">
                     <h4 class="text-center">
-                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
-                           href="#collapse4" style="color:#ffffff;">
-                            @joypixels(':rotating_light:') {{ __('blocks.new-news') }} {{ $article->created_at->diffForHumans() }}
-                            @joypixels(':rotating_light:')
-                        </a>
+                        @joypixels(preg_replace('#\[[^\]]+\]#', '', Str::limit($article->title), 21))
                     </h4>
                 </div>
-                @else
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            <h4 class="text-center">
-                                <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse4" style="color:#ffffff;">
-                                    {{ __('blocks.check-news') }} {{ $article->created_at->diffForHumans() }}
-                                </a>
-                            </h4>
-                        </div>
-                        @endif
-                        <div aria-expanded="{{ ($article->newNews ? 'true' : 'false') }}" id="collapse4"
+                        <div aria-expanded="{{ ($article->newNews ? 'true' : 'false') }}"
                              class="panel-collapse collapse {{ ($article->newNews ? 'in' : '') }}"
                              style="{{ ($article->newNews ? '' : 'height: 0;') }}">
                             <div class="panel-body no-padding">
+                                <p class="text-muted">
+                                    <em>{{ __('articles.published-at') }}
+                                        {{ date('d.m.Y', $article->created_at->getTimestamp()) }} | {{ date('H:m:s', $article->created_at->getTimestamp()) }}</em>
+                                </p>
                                 <div class="news-blocks">
                                     <a href="{{ route('articles.show', ['id' => $article->id]) }}"
                                        style=" float: right; margin-right: 10px;">
@@ -36,16 +24,6 @@
                                             <img src="{{ url('img/missing-image.png') }}" alt="{{ $article->title }}">
                                         @endif
                                     </a>
-
-                                    <h1 class="text-bold" style="display: inline ;">
-                                        @joypixels(preg_replace('#\[[^\]]+\]#', '', Str::limit($article->title),
-                                        21))
-                                    </h1>
-
-                                    <p class="text-muted">
-                                        <em>{{ __('articles.published-at') }}
-                                            {{ date('d.m.Y', $article->created_at->getTimestamp()) }} | {{ date('H:m:s', $article->created_at->getTimestamp()) }}</em>
-                                    </p>
 
                                     <p style="margin-top: 20px;">
                                         @joypixels(preg_replace('#\[[^\]]+\]#', '', Str::limit($article->content),
