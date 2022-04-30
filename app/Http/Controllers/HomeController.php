56c30636fa5c6ea549d5057784fac520eef66ae0
Yakuza <admin@rm-net.si>
2022-04-30 23:51:00 +0200

    ..::SLOshare::.. - 30.04.2022_23:50 - stats

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,13 @@ use App\Models\PersonalFreeleech;
 use App\Models\Poll;
 use App\Models\Post;
 use App\Models\Topic;
+//SLOshare
+use App\Models\Category;
+use App\Models\History;
+use App\Models\Language;
+use App\Models\Peer;
+use App\Models\TorrentRequest;
+//SLOshare
 use App\Models\Torrent;
 use App\Models\User;
 use Carbon\Carbon;
@@ -183,6 +190,84 @@ class HomeController extends Controller
         $freeleechTokens = FreeleechToken::where('user_id', $user->id)->get();
         $bookmarks = Bookmark::where('user_id', $user->id)->get();
 
+//SLOshare
+        // Total Members Count (All Groups)
+        $allUser = \cache()->remember('all_user', $this->carbon, fn () => User::withTrashed()->count());
+
+        // Total Active Members Count (Not Validating, Banned, Disabled, Pruned)
+        $activeUser = \cache()->remember('active_user', $this->carbon, function () {
+            $bannedGroup = \cache()->rememberForever('banned_group', fn () => Group::where('slug', '=', 'banned')->pluck('id'));
+            $validatingGroup = \cache()->rememberForever('validating_group', fn () => Group::where('slug', '=', 'validating')->pluck('id'));
+            $disabledGroup = \cache()->rememberForever('disabled_group', fn () => Group::where('slug', '=', 'disabled')->pluck('id'));
+            $prunedGroup = \cache()->rememberForever('pruned_group', fn () => Group::where('slug', '=', 'pruned')->pluck('id'));
+
+            return User::whereIntegerNotInRaw('group_id', [$validatingGroup[0], $bannedGroup[0], $disabledGroup[0], $prunedGroup[0]])->count();
+        });
+
+        // Total Disabled Members Count
+        $disabledUser = \cache()->remember('disabled_user', $this->carbon, function () {
+            $disabledGroup = \cache()->rememberForever('disabled_group', fn () => Group::where('slug', '=', 'disabled')->pluck('id'));
+
+            return User::where('group_id', '=', $disabledGroup[0])->count();
+        });
+
+        // Total Pruned Members Count
+        $prunedUser = \cache()->remember('pruned_user', $this->carbon, function () {
+            $prunedGroup = \cache()->rememberForever('pruned_group', fn () => Group::where('slug', '=', 'pruned')->pluck('id'));
+
+            return User::onlyTrashed()->where('group_id', '=', $prunedGroup[0])->count();
+        });
+
+        // Total Banned Members Count
+        $bannedUser = \cache()->remember('banned_user', $this->carbon, function () {
+            $bannedGroup = \cache()->rememberForever('banned_group', fn () => Group::where('slug', '=', 'banned')->pluck('id'));
+
+            return User::where('group_id', '=', $bannedGroup[0])->count();
+        });
+
+        // Total Torrents Count
+        $numTorrent = \cache()->remember('num_torrent', $this->carbon, fn () => Torrent::count());
+
+        // Total Categories With Torrent Count
+        $categories = Category::withCount('torrents')->get()->sortBy('position');
+
+        // Total HD Count
+        $numHd = \cache()->remember('num_hd', $this->carbon, fn () => Torrent::where('sd', '=', 0)->count());
+
+        // Total SD Count
+        $numSd = \cache()->remember('num_sd', $this->carbon, fn () => Torrent::where('sd', '=', 1)->count());
+
+        // Total Torrent Size
+        $torrentSize = \cache()->remember('torrent_size', $this->carbon, fn () => Torrent::sum('size'));
+
+        // Total Seeders
+        $numSeeders = \cache()->remember('num_seeders', $this->carbon, fn () => Peer::where('seeder', '=', 1)->count());
+
+        // Total Leechers
+        $numLeechers = \cache()->remember('num_leechers', $this->carbon, fn () => Peer::where('seeder', '=', 0)->count());
+
+        // Total Peers
+        $numPeers = \cache()->remember('num_peers', $this->carbon, fn () => Peer::count());
+
+        //Total Upload Traffic Without Double Upload
+        $actualUpload = \cache()->remember('actual_upload', $this->carbon, fn () => History::sum('actual_uploaded'));
+
+        //Total Upload Traffic With Double Upload
+        $creditedUpload = \cache()->remember('credited_upload', $this->carbon, fn () => History::sum('uploaded'));
+
+        //Total Download Traffic Without Freeleech
+        $actualDownload = \cache()->remember('actual_download', $this->carbon, fn () => History::sum('actual_downloaded'));
+
+        //Total Download Traffic With Freeleech
+        $creditedDownload = \cache()->remember('credited_download', $this->carbon, fn () => History::sum('downloaded'));
+
+        //Total Up/Down Traffic without perks
+        $actualUpDown = $actualUpload + $actualDownload;
+
+        //Total Up/Down Traffic with perks
+        $creditedUpDown = $creditedUpload + $creditedDownload;
+//SLOshare
+
         return \view('home.index', [
             'user'               => $user,
             'personal_freeleech' => $personalFreeleech,
@@ -211,6 +296,27 @@ class HomeController extends Controller
             'past_uploaders'     => $pastUploaders,
             'freeleech_tokens'   => $freeleechTokens,
             'bookmarks'          => $bookmarks,
+//SLOshare
+            'all_user'          => $allUser,
+            'active_user'       => $activeUser,
+            'disabled_user'     => $disabledUser,
+            'pruned_user'       => $prunedUser,
+            'banned_user'       => $bannedUser,
+            'num_torrent'       => $numTorrent,
+            'categories'        => $categories,
+            'num_hd'            => $numHd,
+            'num_sd'            => $numSd,
+            'torrent_size'      => $torrentSize,
+            'num_seeders'       => $numSeeders,
+            'num_leechers'      => $numLeechers,
+            'num_peers'         => $numPeers,
+            'actual_upload'     => $actualUpload,
+            'actual_download'   => $actualDownload,
+            'actual_up_down'    => $actualUpDown,
+            'credited_upload'   => $creditedUpload,
+            'credited_download' => $creditedDownload,
+            'credited_up_down'  => $creditedUpDown,
+//SLOshare
         ]);
     }
 }
