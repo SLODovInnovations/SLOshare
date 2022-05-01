909b4fe7edd4112df9f29858874ba5d70abfa3dd
Yakuza <admin@rm-net.si>
2022-05-01 23:50:52 +0200

    ..::SLOshare::.. - 01.05.2022_23:50

--- resources/views/partials/top_nav.blade.php
+++ resources/views/partials/top_nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="top-nav" x-data="{ expanded: false }" x-bind:class="expanded && 'mobile'">
     <div class="top-nav__left">
         <a class="top-nav__branding" href="{{ route('home.index') }}">
-            <span class="top-nav__site-logo"><img src="{{ url('/logo-footer.png') }}"></span>
+            <span class="top-nav__site-logo"><img src="{{ url('/logo-footer.png') }}" style="height: 40px;"></span>
         </a>
         <livewire:quick-search-dropdown />
     </div>
