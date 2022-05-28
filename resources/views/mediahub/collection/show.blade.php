9a7ac9923a02d121d4561a4c66245a54e5f8c88b
Yakuza <admin@rm-net.si>
2022-05-28 20:39:24 +0200

    ..::SLOshare::.. - 28.05.2022_20:38

--- resources/views/mediahub/collection/show.blade.php
+++ resources/views/mediahub/collection/show.blade.php
@@ -33,7 +33,7 @@
             <div class="movie-overlay"></div>
 
             <div class="movie-poster">
-                <img src="{{ $collection->poster ? tmdb_image('poster_big', $collection->poster) : '/img/SLOshare/meta_no_image_holder_400x600.jpg' }}"
+                <img src="{{ $collection->poster ? tmdb_image('poster_big', $collection->poster) : '/img/SLOshare/music_no_image_holder_200x300.jpg' }}"
                      class="img-responsive" id="meta-poster">
             </div>
 
