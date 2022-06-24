9d9da5f9401a7556ea93f7bf3de6f6b374441e70
Yakuza <admin@rm-net.si>
2022-06-24 06:59:16 +0200

    ..::SLOshare::.. - 24.06.2022_06:59 - v1.3.0

--- resources/views/emails/activate.blade.php
+++ resources/views/emails/activate.blade.php
@@ -1,7 +1,10 @@
 @component('mail::message')
-# {{ __('email.register-header') }} {{ config('other.title') }} !
-**{{ __('email.register-code') }}**
-@component('mail::button', ['url' => route('activate', $code), 'color' => 'blue'])
+    # {{ __('email.register-header') }} {{ config('other.title') }} !
+    **{{ __('email.register-code') }}**
+    @component('mail::button', ['url' => route('activate', $code), 'color' => 'blue'])
+        {{ __('email.activate-account') }}
+    @endcomponent
+    <p>{{ __('email.register-footer') }}</p>
     <br>
     <p>
     Dobrodošli v največjem slovenskem omrežju za prosto izmenjavo informacij! V pozdravnem sporočilu nekaj kratkih navodil,
@@ -59,6 +62,7 @@
     pričakujejo od vas. Naprošamo vas, da si preberete še naš Pravilnik, Pravni pouk in FAQ.
     </p>
     <br>
+    <p style="word-wrap: break-word; overflow-wrap: break-word; word-break: break-word;">{{ route('activate', $code) }}</p>
     <br>
     <p>
     Delimo, povejmo naprej in se skupaj družimo in zabavajmo :)
@@ -69,13 +73,10 @@
     </p>
     <br>
     <br>
+    <br>
     <p>Lep pozdrav,</p>
     <p>Ekipa {{ config('other.title') }}</p>
     <br>
     <p>Na voljo smo Vam tudi po E-Mail naslovu v primeru težave ali predlogov.</p>
     <p>E-Mail: <a href="mailto:info@sloshare.eu">info@sloshare.eu</a>
-{{ __('email.activate-account') }}
-@endcomponent
-<p>{{ __('email.register-footer') }}</p>
-<p style="word-wrap: break-word; overflow-wrap: break-word; word-break: break-word;">{{ route('activate', $code) }}</p>
 @endcomponent
