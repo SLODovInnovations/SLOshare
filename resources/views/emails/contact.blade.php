7f869b1836e7baf81ec7e29b6546c14e96f0aa4b
Yakuza <admin@rm-net.si>
2022-07-09 13:26:58 +0200

    ..::SLOshare::.. - 09.07.2022_13:26 - v1.3.2

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -5,10 +5,11 @@
 
 
 Lep pozdrav,
-
+<br>
 Ekipa {{ config('other.title') }}
 
 
 Na voljo smo Vam tudi po E-Mail naslovu v primeru težave ali predlogov.
+<br>
 E-Mail: <a href="mailto:info@sloshare.eu">info@sloshare.eu</a>
 @endcomponent
--- resources/views/emails/deny_application.blade.php
+++ resources/views/emails/deny_application.blade.php
@@ -5,10 +5,11 @@ Vaša prošnja je bila zavrnjena iz naslednjega razloga:
 
 
 Lep pozdrav,
-
+<br>
 Ekipa {{ config('other.title') }}
 
 
 Na voljo smo Vam tudi po E-Mail naslovu v primeru težave ali predlogov.
+<br>
 E-Mail: <a href="mailto:info@sloshare.eu">info@sloshare.eu</a>
 @endcomponent
