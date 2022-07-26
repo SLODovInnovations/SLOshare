a81820d190b3a95eac6522f0079097938600a9ff
Yakuza <admin@rm-net.si>
2022-07-26 21:05:59 +0200

    ..::SLOshare::.. - 26.07.2022_21:05 - v1.3.3

--- resources/js/sloshare/custom.js
+++ resources/js/sloshare/custom.js
@@ -154,37 +154,7 @@ $(document).ready(function () {
 var slider = new KeenSlider(
         "#SLOCarousel",
         {
-          loop: true,
-        },
-        [
-          (slider) => {
-            let timeout
-            let mouseOver = false
-            function clearNextTimeout() {
-              clearTimeout(timeout)
-            }
-            function nextTimeout() {
-              clearTimeout(timeout)
-              if (mouseOver) return
-              timeout = setTimeout(() => {
-                slider.next()
-              }, 2000)
-            }
-            slider.on("created", () => {
-              slider.container.addEventListener("mouseover", () => {
-                mouseOver = true
-                clearNextTimeout()
-              })
-              slider.container.addEventListener("mouseout", () => {
-                mouseOver = false
-                nextTimeout()
-              })
-              nextTimeout()
-            })
-            slider.on("dragStarted", clearNextTimeout)
-            slider.on("animationEnded", nextTimeout)
-            slider.on("updated", nextTimeout)
-          },
-        ]
-      )
+        slidesPerView: 3,
+        spacing: 15
+        }
 });
