be3619e47a5874deb0483fa3b0bf214a87a5627b
Yakuza <admin@rm-net.si>
2022-08-18 21:57:04 +0200

    ..::SLOshare::.. - 18.08.2022_21:57 - v1.3.3 - cartoons_meta -2

--- database/migrations/2022_08_18_171858_update_categories_table.php
+++ database/migrations/2022_08_18_171858_update_categories_table.php
@@ -14,6 +14,7 @@ return new class() extends Migration {
     {
         Schema::table('categories', function (Blueprint $table) {
             $table->boolean('cartoons_meta')->default(0)->after('meta');
+            $table->dropColumn('meta');
         });
     }
 };
