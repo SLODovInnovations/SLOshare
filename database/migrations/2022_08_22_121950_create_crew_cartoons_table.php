a51a951be99d64b82babd563fccf3f80adb48f62
Yakuza <admin@rm-net.si>
2022-08-20 12:28:31 +0200

    ..::SLOshare::.. - 20.08.2022_12:28 - v1.3.3 - cartoons_meta -18

---
+++ database/migrations/2022_08_22_121950_create_crew_cartoons_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('crew_cartoons', function (Blueprint $table) {
+            $table->unsignedInteger('cartoons_id');
+            $table->unsignedInteger('person_id');
+            $table->primary(['cartoons_id', 'person_id']);
+        });
+    }
+};
