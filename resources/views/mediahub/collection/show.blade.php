581123ab7cc421ba9ee69c0509504f8dab0bf934
Yakuza <admin@rm-net.si>
2022-08-27 22:52:59 +0200

    ..::SLOshare::.. - 27.08.2022_22:52 - v1.3.3 - cartoons_meta -65

--- resources/views/mediahub/collection/show.blade.php
+++ resources/views/mediahub/collection/show.blade.php
@@ -120,6 +120,29 @@
                                         </div>
                                     </div>
                                 @endforeach
+                                @foreach($collection->show->sortBy('release_date') as show)
+                                    <div class="item mini backdrop mini_card col-md-3">
+                                        <div class="image_content">
+                                            @php
+                                                $torrent_temp = App\Models\Torrent::where('tmdb', '=', show->id)
+                                                ->whereIn('category_id', function ($query) {
+                                                $query->select('id')->from('categories')->where('tv_meta', '=', true);
+                                                })->first()
+                                            @endphp
+                                            <a href="{{ route('torrents.similar', ['category_id' => $torrent_temp->category_id, 'tmdb' => $how->id]) }}">
+                                                <div>
+                                                    <img class="backdrop"
+                                                         src="{{ tmdb_image('poster_mid', $show->poster) }}">
+                                                </div>
+                                                <div style=" margin-top: 8px;">
+                                                    <span class="badge-extra"><i
+                                                                class="fas fa-calendar text-purple"></i> {{ __('common.year') }}: {{ substr($show->release_date, 0, 4) }}</span>
+                                                    <span class="badge-extra"><i class="fas fa-star text-gold"></i> {{ __('torrent.rating') }}: {{ $show->vote_average }}</span>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
                             </section>
                         </td>
                     </tr>
