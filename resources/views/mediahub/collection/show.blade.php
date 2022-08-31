74546c13678cff08a478b0b5b3f8f30d7c3aeb52
Yakuza <admin@rm-net.si>
2022-08-31 06:56:17 +0200

    ..::SLOshare::.. - 20.08.2022_06:56 - v1.3.3 - cartoons_meta -79

--- resources/views/mediahub/collection/show.blade.php
+++ resources/views/mediahub/collection/show.blade.php
@@ -64,46 +64,16 @@
 
         <br>
 
-        <div class="panel panel-chat shoutbox">
-            <div class="panel-heading">
-                <h4><i class="{{ config("other.font-awesome") }} fa-film"></i> Film</h4>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-condensed table-bordered table-striped">
-                    <tbody>
-                    <tr>
-                        <td>
-                            <section class="recommendations">
-                                @foreach($collection->movie->sortBy('release_date') as $movie)
-                                    <div class="item mini backdrop mini_card col-md-3">
-                                        <div class="image_content">
-                                            @php
-                                                $torrent_temp = App\Models\Torrent::where('tmdb', '=', $movie->id)
-                                                ->whereIn('category_id', function ($query) {
-                                                $query->select('id')->from('categories')->where('movie_meta', '=', true);
-                                                })->first()
-                                            @endphp
-                                            <a href="{{ route('torrents.similar', ['category_id' => $torrent_temp->category_id, 'tmdb' => $movie->id]) }}">
-                                                <div>
-                                                    <img class="backdrop"
-                                                         src="{{ tmdb_image('poster_mid', $movie->poster) }}">
-                                                </div>
-                                                <div style=" margin-top: 8px;">
-                                                    <span class="badge-extra"><i
-                                                                class="fas fa-calendar text-purple"></i> {{ __('common.year') }}: {{ substr($movie->release_date, 0, 4) }}</span>
-                                                    <span class="badge-extra"><i class="fas fa-star text-gold"></i> {{ __('torrent.rating') }}: {{ $movie->vote_average }}</span>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </section>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        {{-- Movie Meta Block --}}
+        @if ($torrent->category->movie_meta)
+            @include('mediahub.collection.partials.movie_meta_collection')
+        @endif
+
+        {{-- Cartoon Meta Block --}}
+        @if ($torrent->category->cartoon_meta)
+            @include('mediahub.collection.partials.cartoon_meta_collection')
+        @endif
+
     </div>
 
     <div class="torrent box container" id="comments">
@@ -189,18 +159,20 @@
                     <div class="form-group">
                         <label for="content">{{ __('common.your-comment') }}:</label>
                         <span class="badge-extra">BBCode {{ __('common.is-allowed') }}</span>
-                        <textarea id="content" name="content" cols="30" rows="5" class="form-control"></textarea>
+                        <span class="pull-right" x-data="{ emoji: false }">
+                            <img src="{{ url('img/emoji-add.png') }}" width="32px" x-on:click="emoji = ! emoji">
+
+                            <div style="position: absolute; z-index: 1;" x-show="emoji" @click.away="emoji = false">
+                                <emoji-picker></emoji-picker>
+                            </div>
+                        </span>
+                        <textarea id="editor" name="content" cols="30" rows="5" class="form-control"></textarea>
                     </div>
                     <button type="submit" class="btn btn-danger">{{ __('common.submit') }}</button>
-
-@if (auth()->user()->group->is_admin)
                     <label class="radio-inline"><strong>{{ __('common.anonymous') }} {{ __('common.comment') }}
                             :</strong></label>
                     <input type="radio" value="1" name="anonymous"> {{ __('common.yes') }}
                     <input type="radio" value="0" checked="checked" name="anonymous"> {{ __('common.no') }}
-@else
-                    <input type="hidden" name="anonymous" value="0">
-@endif
                 </form>
             </div>
             <!-- /Add comment -->
