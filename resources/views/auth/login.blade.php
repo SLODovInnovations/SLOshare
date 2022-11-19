d0918f17c26c7fc5ad8720582e61dd41bd907252
Yakuza <admin@rm-net.si>
2022-11-19 11:00:23 +0100

    ..::SLOshare::.. - 19.11.2022_11:00 - v1.4.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,15 +22,15 @@
     <link rel="stylesheet" href="{{ mix('css/main/login.css') }}" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/snowfall/font-awesome.css') }}" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/snowfall/jqueryscripttop.css') }}" crossorigin="anonymous">
-    <script src="{{ mix('js/jquery.snowfall.js') }}" crossorigin="anonymous"></script>
     <script src="{{ mix('js/jquery-1.12.4.js') }}" crossorigin="anonymous"></script>
+    <script src="{{ mix('js/jquery.snowfall.js') }}" crossorigin="anonymous"></script>
+    <script src="{{ mix('js/snowfall.js') }}" crossorigin="anonymous"></script>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q8PXB7XS8X"></script>
 <script src="{{ mix('js/google.js') }}" crossorigin="anonymous"></script>
 <!-- Google tag (gtag.js) -->
 <script type="text/javascript" async="" src="https://adserver.adman.si/add/adman.js"></script>
 <script src="{{ mix('js/ad.js') }}" crossorigin="anonymous"></script>
-<script src="{{ mix('js/snowfall.js') }}" crossorigin="anonymous"></script>
 </head>
 
 @php $bg = rand(1, 38); $bgchange = $bg.".jpg" @endphp
