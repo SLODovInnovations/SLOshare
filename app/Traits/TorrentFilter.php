a565a4042d88e1b639059b2418c4dc541c6f2234
Yakuza <admin@rm-net.si>
2022-11-20 13:29:32 +0100

    ..::SLOshare::.. - 20.11.2022_13:11 - v1.4.2

--- app/Traits/TorrentFilter.php
+++ app/Traits/TorrentFilter.php
@@ -100,7 +100,7 @@ trait TorrentFilter
                 ->orWhere(
                     fn ($query) => $query
                     ->whereIn('category_id', Category::select('id')->where('cartoontv_meta', '=', 1))
-                    ->whereIn('tmdb', DB::table('cartoontv_genre')->select('cartoontv_id')->whereIn('genre_id', $genres))
+                    ->whereIn('tmdb', DB::table('genre_cartoontv')->select('cartoontv_id')->whereIn('genre_id', $genres))
                 )
             );
     }
@@ -153,7 +153,7 @@ trait TorrentFilter
             ->whereIn('tmdb', DB::table('cartoon_collection')->select('cartoon_id')->where('collection_id', '=', $collectionId));
 
             ->whereIn('category_id', Category::select('id')->where('cartoontv_meta', '=', 1))
-            ->whereIn('tmdb', DB::table('cartoontv_collection')->select('cartoontv_id')->where('collection_id', '=', $collectionId));
+            ->whereIn('tmdb', DB::table('collection_cartoontv')->select('cartoontv_id')->where('collection_id', '=', $collectionId));
     }
 
 
