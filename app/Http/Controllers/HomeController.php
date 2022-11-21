d53f73fcd861e1a81f1f7fdd9ff93c119dd5cf59
Yakuza <admin@rm-net.si>
2022-11-21 18:37:38 +0100

    ..::SLOshare::.. - 21.11.2022_18:37 - v1.4.2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -185,7 +185,7 @@ class HomeController extends Controller
             ->get());
 
         //SLOshare
-        $allUser = \cache()->remember('all_user', $this->carbon, fn () => User::withTrashed()->count())
+        $allUser = \cache()->remember('all_user', $this->carbon, fn () => User::withTrashed()->count());
         //SLOshare
 
         $freeleechTokens = FreeleechToken::where('user_id', $user->id)->get();
