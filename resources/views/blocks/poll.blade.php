e70e0799b977ae1482972945b08d1615d46cb12e
Yakuza <admin@rm-net.si>
2022-11-28 20:56:31 +0100

    ..::SLOshare::.. - 28.11.2022_20:56 - v1.4.2

--- resources/views/blocks/poll.blade.php
+++ resources/views/blocks/poll.blade.php
@@ -1,4 +1,4 @@
-<section class="panelV2">
+    <section class="panelV2">
 @if ($poll && $poll->voters->where('user_id', '=', auth()->user()->id)->isEmpty())
         <div class="panel panel-danger">
             <div class="panel-heading">
@@ -8,9 +8,8 @@
                     </div>
                 </h4>
             </div>
-			<h2 class="panel__heading">{{ $poll->title }}</h2>
             <div class="panel-body">
-
+                <h2 class="panel__heading">{{ $poll->title }}</h2>
                 <form class="form-horizontal" method="POST" action="/polls/vote">
                     @csrf
                     @if (count($errors) > 0)
@@ -56,35 +55,7 @@
                 @endif
             </div>
         </div>
-@endif
-@if($total = $poll->options->sum('votes'))
-        <div class="panel panel-danger">
-            <div class="panel-heading">
-                <h4 class="text-center">
-                    <div class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" style="color:#ffffff;">
-                        <i class="{{ config('other.font-awesome') }} fa-chart-pie"></i> {{ __('poll.poll') }}
-                    </div>
-                </h4>
-            </div>
-			<h2 class="panel__heading">{{ $poll->title }}</h2>
-            <div class="panel-body">
-            @foreach ($poll->options as $option)
-                <p class="form__group">
-                    <label class="form__label" for="option{{ $loop->iteration }}">
-                        {{ $option->name }} ({{ \number_format($total === 0 ? 0 : 100 * $option->votes / $total, 2) }}%)
-                    </label>
-                    <meter
-                        id="option{{ $loop->iteration }}"
-                        class="form__meter"
-                        min="0"
-                        max="{{ $total }}"
-                        value="{{ $option->votes }}"
-                    >
-                        {{ $option->votes }} {{ $option->votes === 1 ? __('poll.vote') : __('poll.votes') }}
-                    </meter>
-                </p>
-            @endforeach
-            </div>
-		</div>
+@else
+        @include('poll_vole')
 @endif
 	</section>
