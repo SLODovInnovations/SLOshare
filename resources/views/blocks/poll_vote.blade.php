458844a3fe797f9d043be72289f583b2d1604427
Yakuza <admin@rm-net.si>
2022-11-29 18:09:09 +0100

    ..::SLOshare::.. - 28.11.2022_21:58 - v1.4.2

--- resources/views/blocks/poll_vote.blade.php
+++ resources/views/blocks/poll_vote.blade.php
@@ -1,4 +1,3 @@
-            @php($total = $poll->options->sum('votes'))
             @foreach ($poll->options as $option)
                 <h2 class="panel__heading">{{ $poll->title }}</h2>
                 <p class="form__group">
