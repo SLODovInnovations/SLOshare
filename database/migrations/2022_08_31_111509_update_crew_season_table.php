1cc3806630699c7fb87aef3c37450c83e8347552
Yakuza <admin@rm-net.si>
2023-01-06 15:27:18 +0100

    ..::SLOshare::.. - 06.01.2023_15:27 - v1.4.4

--- database/migrations/2022_08_31_111509_update_crew_season_table.php
+++ database/migrations/2022_08_31_111509_update_crew_season_table.php
@@ -2,6 +2,7 @@
 
 use App\Models\Season;
 use App\Models\Tv;
+use App\Models\CartoonTv;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -24,6 +25,23 @@ return new class () extends Migration {
 
             foreach ($data['seasons'] as $season) {
                 $season = (new \App\Services\Tmdb\Client\Season($tv->id, \sprintf('%02d', $season['season_number'])))->getData();
+                $seasonModel = Season::find($season['id']);
+
+                if (isset($season['credits']['crew'])) {
+                    foreach ($season['credits']['crew'] as $crewMember) {
+                        $seasonModel->crew()->updateExistingPivot($crewMember['id'], [
+                            'department' => $crewMember['department'],
+                            'job'        => $crewMember['job'],
+                        ]);
+                    }
+                }
+            }
+        }
+        foreach (CartoonTv::all() as $cartoontv) {
+            $data = (new \App\Services\Tmdb\Client\CartoonTv($cartoontv->id))->getData();
+
+            foreach ($data['seasons'] as $season) {
+                $season = (new \App\Services\Tmdb\Client\Season($cartoontv->id, \sprintf('%02d', $season['season_number'])))->getData();
                 $seasonModel = Season::find($season['id']);
 
                 if (isset($season['credits']['crew'])) {
