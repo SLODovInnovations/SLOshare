ca36b6b362d384f35dc73751653a0dc19e8760eb
Yakuza <admin@rm-net.si>
2023-01-09 19:39:03 +0100

    ..::SLOshare::.. - 09.01.2023_19:38 - v1.4.4 - TEST

--- resources/views/blocks/ad_right.blade.php
+++ resources/views/blocks/ad_right.blade.php
@@ -1,4 +1,5 @@
 	<section class="panelV2">
+	    <script src="{{ mix('js/ad.js') }}" crossorigin="anonymous"></script>
 		<div style="width: 300px; height: 600px;" data-admanids="f57b6946_d34b7921"></div>
 	</section>
 </aside>
