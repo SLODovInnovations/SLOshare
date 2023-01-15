d6f0a5fdf71d1b7f26d6bed9c938300700ecd6bd
Yakuza <admin@rm-net.si>
2023-01-15 14:03:10 +0100

    ..::SLOshare::.. - 15.01.2023_14:03 - v1.4.4

---
+++ app/Services/Tmdb/Client/FindCartoonTv.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\Tmdb\Client;
+
+class FindCartoonTv
+{
+    public \GuzzleHttp\Client $client;
+
+    final public const API_BASE_URI = 'https://api.TheMovieDB.org/3';
+
+    public $data;
+
+    public function __construct($query, $year)
+    {
+        $this->client = new \GuzzleHttp\Client(
+            [
+                'base_uri'    => self::API_BASE_URI,
+                'verify'      => false,
+                'http_errors' => false,
+                'headers'     => [
+                    'Content-Type' => 'application/json',
+                    'Accept'       => 'application/json',
+                ],
+                // 'query' => [
+                //     'api_key' => config('api-keys.tmdb'),
+                //     'query' => $query,
+                // ],
+            ]
+        );
+
+        $response = $this->client->request('get', 'https://api.themoviedb.org/3/search/tv?api_key='.\config('api-keys.tmdb').'&query='.$query.'&first_air_date_year='.$year);
+
+        $this->data = \json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function get_homepage()
+    {
+        return $this->data['homepage'];
+    }
+}
