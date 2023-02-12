7dcb7ce578a716a6bb25011a6775ea4822f3356e
Yakuza <admin@rm-net.si>
2023-02-12 14:25:16 +0100

    ..::SLOshare::.. - 12.02.2023_14:25 - v1.4.4

--- resources/views/livewire/includes/_tooltip.blade.php
+++ resources/views/livewire/includes/_tooltip.blade.php
@@ -5,24 +5,61 @@
         </div>
         <div class="card_body">
             <div class="body_poster">
-                @if ($torrent->category->movie_meta || $torrent->category->tv_meta || $torrent->category->cartoon_meta || $torrent->category->cartoontv_meta)
-                    <img src="{{ isset($meta->poster) ? tmdb_image('poster_big', $meta->poster) : 'https://via.placeholder.com/600x900' }}"
-                         class="show-poster" alt="{{ __('torrent.poster') }}">
+                @if ($torrent->category->movie_meta || $torrent->category->tv_meta)
+                    @if(file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                        <img src="{{ url('files/img/torrent-cover_' . $torrent->id . '.jpg') }}"
+                            class="show-poster"  alt="{{ __('torrent.poster') }}">
+                    @else
+                        <img src="{{ isset($meta->poster) ? tmdb_image('poster_big', $meta->poster) : '/img/movie_no_image_SLOshare/600x900.jpg' }}"
+                            class="show-poster" alt="{{ __('torrent.poster') }}">
+                    @endif
+                @endif
+
+                @if ($torrent->category->cartoon_meta || $torrent->category->cartoontv_meta)
+                    @if(file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                        <img src="{{ url('files/img/torrent-cover_' . $torrent->id . '.jpg') }}"
+                            class="show-poster"  alt="{{ __('torrent.poster') }}">
+                    @else
+                        <img src="{{ isset($meta->poster) ? tmdb_image('poster_big', $meta->poster) : '/img/SLOshare/cartoon_no_image_600x900.jpg' }}"
+                            class="show-poster" alt="{{ __('torrent.poster') }}">
+                    @endif
                 @endif
 
                 @if ($torrent->category->game_meta && isset($meta) && $meta->cover->image_id && $meta->name)
-                    <img src="https://images.igdb.com/igdb/image/upload/t_original/{{ $meta->cover->image_id }}.jpg"
-                         class="show-poster"
-                         data-name='<i style="color: #a5a5a5;">{{ $meta->name ?? 'N/A' }}</i>'
-                         data-image='<img src="https://images.igdb.com/igdb/image/upload/t_original/{{ $meta->cover->image_id }}.jpg" alt="{{ __('torrent.poster') }}" style="height: 1000px;">'
-                         class="torrent-poster-img-small show-poster" alt="{{ __('torrent.poster') }}">
+                    @if(file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                        <img src="{{ url('files/img/torrent-cover_' . $torrent->id . '.jpg') }}"
+                            class="show-poster"  alt="{{ __('torrent.poster') }}">
+                    @else
+                        <img src="{{ isset($meta->cover) ? 'https://images.igdb.com/igdb/image/upload/t_original/'.$meta->cover['image_id'].'.jpg' : '/img/SLOshare/games_no_image_600x900.jpg' }}"
+                            class="show-poster"
+                            data-name='<i style="color: #a5a5a5;">{{ $meta->name ?? 'N/A' }}</i>'
+                            data-image='<img src="{{ isset($meta->cover) ? 'https://images.igdb.com/igdb/image/upload/t_original/'.$meta->cover['image_id'].'.jpg' : '/img/SLOshare/games_no_image_600x900.jpg' }}" alt="{{ __('torrent.poster') }}" style="height: 1000px;">'
+                            class="torrent-poster-img-small show-poster" alt="{{ __('torrent.poster') }}">
+                    @endif
                 @endif
 
-                @if ($torrent->category->no_meta || $torrent->category->music_meta)
-                    <img src="https://via.placeholder.com/600x900" class="show-poster"
-                         data-name='<i style="color: #a5a5a5;">N/A</i>'
-                         data-image='<img src="https://via.placeholder.com/600x900" alt="{{ __('torrent.poster') }}" style="height: 1000px;">'
-                         class="torrent-poster-img-small show-poster" alt="{{ __('torrent.poster') }}">
+                @if ($torrent->category->music_meta)
+                    @if(file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                        <img src="{{ url('files/img/torrent-cover_' . $torrent->id . '.jpg') }}"
+                            class="torrent-poster-img-small show-poster"  alt="{{ __('torrent.poster') }}">
+                    @else
+                        <img src="/img/SLOshare/music_no_image_600x900.jpg" class="show-poster"
+                            data-name='<i style="color: #a5a5a5;">N/A</i>'
+                            data-image='<img src="/img/SLOshare/music_no_image_600x900.jpg" alt="{{ __('torrent.poster') }}" style="height: 1000px;">'
+                            class="torrent-poster-img-small show-poster" alt="{{ __('torrent.poster') }}">
+                    @endif
+                @endif
+
+                @if ($torrent->category->no_meta)
+                    @if(file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg'))
+                        <img src="{{ url('files/img/torrent-cover_' . $torrent->id . '.jpg') }}"
+                            class="torrent-poster-img-small show-poster"  alt="{{ __('torrent.poster') }}">
+                    @else
+                        <img src="/img/SLOshare/meta_no_image_600x900.jpg" class="show-poster"
+                            data-name='<i style="color: #a5a5a5;">N/A</i>'
+                            data-image='<img src="/img/SLOshare/meta_no_image_600x900.jpg" alt="{{ __('torrent.poster') }}" style="height: 1000px;">'
+                            class="torrent-poster-img-small show-poster" alt="{{ __('torrent.poster') }}">
+                    @endif
                 @endif
             </div>
             <div class="body_description">
