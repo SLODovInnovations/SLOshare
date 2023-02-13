b81de4418ce85a05e233e42df2a9d534ad1fe2d3
Yakuza <admin@rm-net.si>
2023-02-13 07:48:55 +0100

    ..::SLOshare::.. - 13.02.2023_07:48 - v1.4.4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -123,7 +123,7 @@ class HomeController extends Controller
 
         $cartoonrecommended = cache()->remember('cartoonrecommended_torrents', $expiresAt, fn () => Torrent::with(['user', 'category', 'type', 'resolution'])
             ->withCount(['thanks', 'comments'])
-            ->whereIn('category_id', '=', [3, 4])
+            ->whereIn('category_id', [3, 4])
             ->where('sticky', '=', 1)
             ->latest()
             ->take(5)
